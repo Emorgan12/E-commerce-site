58940430854e9ac928f4716b2e0065586a0b3742
Elliot <140021772+Emorgan12@users.noreply.github.com>
2024-10-23 09:56:56 +0100

    doing things hopefully

--- Frontend/e-commerce-site/src/index.jsx
+++ Frontend/e-commerce-site/src/index.jsx
@@ -3,19 +3,30 @@ import reactDOM from "react-dom";
 import { useState, useEffect } from "react";
 
 function LandingPage() {
+    useEffect(() => {
+        console.log(screen.width, screen.height);
+    }, []);
+    
     return (
         <>
         <nav>
-            <a href="../index.html"><img id="home" src="https://cdn-icons-png.flaticon.com/128/12891/12891793.png" /></a>
-            <ul>
+            <ul id="mobile">
+                <li><a href="../index.html"><img id="home" className="mobile-img" src="https://cdn-icons-png.flaticon.com/128/12891/12891793.png" /></a></li>
+                <li className="container">
+                    <p><b>BRAND NAME</b></p>
+                </li>
+                <li id="cart" ><a href="../cart.html">< img id="cart-img" className="mobile-img" src="https://cdn-icons-png.flaticon.com/128/12599/12599129.png" /></a></li>
+            </ul>
+            <ul id="big-scrn">
+                <li><a href="../index.html"><img id="home" src="https://cdn-icons-png.flaticon.com/128/12891/12891793.png" /></a></li>
                 <li><b>BRAND NAME</b></li>
                 <li>Clothes</li>
                 <li>Toys</li>
                 <li>Baby</li>
                 <li>Electronics</li>
                 <li>Home</li>
+                <li id="cart" ><a href="../cart.html">< img id="cart-img" src="https://cdn-icons-png.flaticon.com/128/12599/12599129.png" /></a></li>
             </ul>
-            <a href="../cart.html"><img id="cart" src="https://cdn-icons-png.flaticon.com/128/12599/12599129.png" /></a>
         </nav>
 
         <div className="content">
