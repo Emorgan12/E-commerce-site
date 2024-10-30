f3b4f4bb51cfe7c0ebe1508ca21e80d0d5732264
Elliot <140021772+Emorgan12@users.noreply.github.com>
2024-10-30 14:57:37 +0000

    trying

--- e-commerce-site/src/cart.jsx
+++ e-commerce-site/src/cart.jsx
@@ -6,22 +6,31 @@ function Cart(){
     return(
         <>
             <nav>
-                <a href="../index.html"><img id="home" src="https://cdn-icons-png.flaticon.com/128/12891/12891793.png" /></a>
-                <ul>
-                    <li><b>BRAND NAME</b></li>
+                <ul id="mobile">
+                    <li id="home"><a href="../index.html"><img className="mobile-img" src="https://cdn-icons-png.flaticon.com/128/12891/12891793.png" /></a></li>
+                    <li className="container">
+                        <p><b>TRADEZY</b></p>
+                    </li>
+                    <li id="cart" ><a href="../cart.html">< img id="cart-img" className="mobile-img" src="https://cdn-icons-png.flaticon.com/128/12599/12599129.png" /></a></li>
+                </ul>
+                <ul id="big-scrn">
+                    <li id="home"><a href="../index.html"><img src="https://cdn-icons-png.flaticon.com/128/12891/12891793.png" /></a></li>
+                    <li><b>TRADEZY</b></li>
                     <li>Clothes</li>
                     <li>Toys</li>
                     <li>Baby</li>
                     <li>Electronics</li>
                     <li>Home</li>
+                    <li id="cart" ><a href="../cart.html">< img id="cart-img" src="https://cdn-icons-png.flaticon.com/128/12599/12599129.png" /></a></li>
                 </ul>
-                <a href="../cart.html"><img id="cart" src="https://cdn-icons-png.flaticon.com/128/12599/12599129.png" /></a>
             </nav>
+
             <div className="content">
-                <div className="item">
-                    <img src="https://cdn-icons-png.flaticon.com/128/649/649730.png"></img>
-                    <p>Name</p>
-                    <p>Shipping Cost</p>
+                <div class="cart-item">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeJQeJyzgAzTEVqXiGe90RGBFhfp_4RcJJMQ&s" />
+                    <p id="cart-name">Name</p>
+                    <p id="cart-colour">Colour</p>
+                    <p id="cart-price">Price</p>
                 </div>
             </div>
         </>
