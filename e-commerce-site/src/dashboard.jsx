56472e265fc2c0073ad250b204518d4757c1c3c8
Elliot <140021772+Emorgan12@users.noreply.github.com>
2024-12-19 10:28:31 +0000

    sobbong on the floor

--- e-commerce-site/src/dashboard.jsx
+++ e-commerce-site/src/dashboard.jsx
@@ -5,20 +5,13 @@ import Footer from './footer.jsx';
 import BASE_URL from '../main.js';
 import {useNavigate, Link } from 'react-router-dom';
 import { AuthContext } from './authcontext.jsx';
-import { DashboardLayout } from '@toolpad/core/DashboardLayout';
-import { AppProvider} from '@toolpad/core/AppProvider'
+
 
 function Dashboard(){
     const {user} = useContext(AuthContext);
 
     return(
         <>
-            <Navbar/>
-            <AppProvider>
-                <DashboardLayout>
-                    <Footer/>
-                </DashboardLayout>
-            </AppProvider>
         </>
     )
 }
