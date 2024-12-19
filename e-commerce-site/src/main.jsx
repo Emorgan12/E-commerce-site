23a059439d1406866d58dc8f366fc57e11543935
Elliot <140021772+Emorgan12@users.noreply.github.com>
2024-12-19 10:32:40 +0000

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- e-commerce-site/src/main.jsx
+++ e-commerce-site/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from '../app.jsx'
+import App from './app.jsx'
 import './style.css'
 import { AuthProvider } from './authcontext.jsx'
 
