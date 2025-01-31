4f70a5f013f38c49eb1a70fb957aef3e0aca72d4
Elliot <140021772+Emorgan12@users.noreply.github.com>
2025-01-31 09:22:37 +0000

    commit

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23a059439d1406866d58dc8f366fc57e11543935")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+440aa0157d5d82a07965942633d0b15128da30f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
